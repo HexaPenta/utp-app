ed4828de95f91ab71ffb6070617e7464395e789c
WilsonCollantesAponte <wilson.aponte000@gmail.com>
2023-09-08 13:31:36 -0500

    V16

--- app/page.jsx
+++ app/page.jsx
@@ -2,9 +2,24 @@
 
 export default function Landing() {
   return (
-    <h1 className=" font-semibold underline decoration-2">
-      Landing-Page
-      {localStorage.idUser ? <div>are a idUSer</div> : <div>Not a id User</div>}
-    </h1>
+    <div className=" bg-white">
+      <div className=" w-1/2 mx-auto  py-24">
+        <div className=" grid grid-cols-6 animate-pulse gap-y-8 gap-x-3 py-16 pl-12">
+          <div class=" h-4 bg-slate-400/70 rounded-lg col-span-full col-start-1"></div>
+          <div class=" h-4 bg-slate-400/70 rounded-lg col-span-3 "></div>
+          <div class=" h-4 bg-slate-400/70 rounded-lg col-span-2 col-start-1"></div>
+          <div class=" h-4 bg-slate-400/70 rounded-lg col-span-4 col-start-1"></div>
+          <div class=" h-4 bg-slate-400/70 rounded-lg col-span-3 col-start-1"></div>
+          <div class=" h-4 bg-slate-400/70 rounded-lg col-span-3 "></div>
+          <div class=" h-4 bg-slate-400/70 rounded-lg col-span-2 col-start-1"></div>
+          <div class=" h-4 bg-slate-400/70 rounded-lg col-span-2 "></div>
+          <div class=" h-4 bg-slate-400/70 rounded-lg col-span-2 "></div>
+          <div class=" h-4 bg-slate-400/70 rounded-lg col-span-4 col-start-1"></div>
+          <div class=" h-4 bg-slate-400/70 rounded-lg col-span-4 col-start-1"></div>
+          <div class=" h-4 bg-slate-400/70 rounded-lg col-span-4 col-start-1"></div>
+          <div class=" h-4 bg-slate-400/70 rounded-lg col-span-full col-start-1"></div>
+        </div>
+      </div>
+    </div>
   );
 }
