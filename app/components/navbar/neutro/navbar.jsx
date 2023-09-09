9185a7246c0bf8c94dd61707afbbf9c68c2095b5
WilsonCollantesAponte <wilson.aponte000@gmail.com>
2023-09-09 09:12:44 -0500

    V21

---
+++ app/components/navbar/neutro/navbar.jsx
@@ -0,0 +1,88 @@
+"use client";
+
+import Link from "next/link";
+// import DynamicPart from "./dynamicPart";
+
+const idUser =
+  typeof window !== "undefined" ? localStorage.getItem("idUser") : "";
+
+export default function Navbar() {
+  return (
+    <div className=" grid grid-cols-10 bg-indigo-100 gap-5 py-6 px-4 text-center">
+      <button className=" box-border">
+        <img
+          className=" rounded scale-150 ml-7  "
+          src="https://visualizate.utpxpedition.com/sites/default/files/2020-02/logo-visualizate.png"
+          alt=""
+        />
+      </button>
+      <button className=" bg-red-600 col-start-7 col-span-1 rounded text-white hover:bg-indigo-500 text-sm font-semibold px-3 py-2 box-border bg-redUTP">
+        About
+      </button>
+      <Link
+        className=" bg-red-600 rounded col-span-1 text-white hover:bg-indigo-500 text-sm font-semibold px-3 py-2 box-border"
+        href="/post/all"
+      >
+        Posts
+      </Link>
+
+      {/* <DynamicPart /> */}
+
+      <Link
+        className={
+          !idUser
+            ? "hidden"
+            : " bg-red-600 rounded  text-white hover:bg-indigo-500 text-sm font-semibold px-3 py-2 box-border"
+        }
+        href="/user/form/signIn"
+      >
+        Sign in
+      </Link>
+
+      <Link
+        className={
+          idUser
+            ? "hidden"
+            : " bg-red-600 rounded  text-white hover:bg-indigo-500 text-sm font-semibold px-3 py-2 box-border"
+        }
+        href="/post/all"
+      >
+        Dash
+      </Link>
+
+      {/* <Link
+        className=" bg-red-600 rounded text-white hover:bg-indigo-500 text-sm font-semibold px-3 py-2 box-border"
+        // href="https://www.instagram.com/reels/CwYDETrqej3/"
+        href="/user/form/login"
+      >
+        login
+      </Link> */}
+
+      <Link
+        className={
+          Boolean(idUser)
+            ? "hidden"
+            : " block bg-red-600 rounded  text-white hover:bg-indigo-500 text-sm font-semibold px-3 py-2 box-border"
+        }
+        href="/user/form/login"
+      >
+        Login
+      </Link>
+
+      <button
+        className={
+          Boolean(idUser)
+            ? " block bg-red-600 rounded  text-white hover:bg-indigo-500 text-sm font-semibold px-3 py-2 box-border"
+            : " hidden"
+        }
+        onClick={() => {
+          localStorage.clear();
+          //   location.replace("/post/all");
+          location.reload();
+        }}
+      >
+        Logout
+      </button>
+    </div>
+  );
+}
