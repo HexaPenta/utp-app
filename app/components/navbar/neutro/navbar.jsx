53c31ca5f18e9df8930777a52a995e562a566ff2
WilsonCollantesAponte <wilson.aponte000@gmail.com>
2023-09-13 11:00:22 -0500

    V27

--- app/components/navbar/neutro/navbar.jsx
+++ app/components/navbar/neutro/navbar.jsx
@@ -33,13 +33,13 @@ export default function Navbar() {
 
   return (
     <div className=" grid grid-cols-10 bg-indigo-100 gap-5 py-6 px-4 text-center">
-      <button className=" box-border">
+      <Link className=" box-border" href="./">
         <img
           className=" rounded scale-150 ml-7  "
           src="https://visualizate.utpxpedition.com/sites/default/files/2020-02/logo-visualizate.png"
-          alt=""
+          alt="utp-mainImage"
         />
-      </button>
+      </Link>
       <button className=" bg-red-600 col-start-7 col-span-1 rounded text-white hover:bg-indigo-500 text-sm font-semibold px-3 py-2 box-border bg-redUTP">
         About
       </button>
