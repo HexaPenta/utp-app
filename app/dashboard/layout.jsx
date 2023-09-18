5445acaf3f96b9ecc46a5d933cc51fa5a34263c5
WilsonCollantesAponte <wilson.aponte000@gmail.com>
2023-09-17 21:33:45 -0500

    V32

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -3,23 +3,29 @@ import Link from "next/link";
 export default function DashboardLayout({ children }) {
   return (
     <section>
-      <div className=" grid grid-cols-12 bg-indigo-100 gap-5 py-6 px-4 text-center border-y  border-gray-400 pb-6">
-        <div className=" flex bg-orange-500 col-span-2 justify-stretch">
+      <div className=" grid grid-cols-12 bg-indigo-100 gap-5 py-6 px-4 text-center border-y  border-gray-400 pb-6 ">
+        <div className=" flex col-span-2 divide-x-2 divide-gray-500 border-2 rounded border-gray-600">
           <Link
-            // className=" border-x-2 border-y-2  border-gray-600  col-span-2 rounded hover:bg-black  hover:text-white text-sm  font-extrabold font-serif px-3 py-2 box-border text-gray-900"
-            className="   col-span-2 rounded hover:bg-black  hover:text-white text-sm  font-extrabold font-serif px-3 py-2 box-border text-gray-900 w-40 bg-red-600"
+            className=" col-span-2 bg-black  text-white text-sm  font-extrabold font-serif px-3 py-2 box-border w-40 hover:bg-slate-600"
             href="/dashboard/users"
           >
             Users
           </Link>
-          <button className=" bg-green-600 text-2xl grow">+</button>
+          <button className=" text-2xl grow font-bold text-white bg-black hover:bg-slate-600">
+            +
+          </button>
         </div>
-        <Link
-          className=" border-x-2 border-y-2  border-gray-600  col-span-2 rounded hover:bg-black  hover:text-white text-sm  font-extrabold font-serif px-3 py-2 box-border text-gray-900"
-          href="/dashboard/posts"
-        >
-          Posts
-        </Link>
+        <div className=" flex col-span-2 divide-x-2 divide-gray-500 border-2 rounded border-gray-600">
+          <Link
+            className=" col-span-2 bg-black  text-white text-sm  font-extrabold font-serif px-3 py-2 box-border w-40 hover:bg-slate-600"
+            href="/dashboard/posts"
+          >
+            Posts
+          </Link>
+          <button className=" text-2xl grow font-bold text-white bg-black hover:bg-slate-600">
+            +
+          </button>
+        </div>
       </div>
       {children}
     </section>
