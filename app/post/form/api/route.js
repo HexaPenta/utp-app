7d61aa542d8cc1a44b51254773eea3a78c2e0d39
Wilson Collantes Aponte <wilson.aponte000@gmail.com>
2023-12-31 16:46:25 -0500

    Changue only to Post -> post//form/api

--- app/post/form/api/route.js
+++ app/post/form/api/route.js
@@ -37,7 +37,7 @@ export async function POST(request) {
       status: 200,
       headers: {
         "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Methods": "POST",
         "Access-Control-Allow-Headers": "Content-Type, Authorization",
       },
     }
